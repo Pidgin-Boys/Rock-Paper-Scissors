0d168f2878075e1d146bb57788595ee989a2dfdd
Lakers2012 <ereedley@yahoo.com>
2012-03-03 07:27:45 -0800

    Update RPS/src/Output.java

--- RPS/src/Output.java
+++ RPS/src/Output.java
@@ -1,7 +1,7 @@
 public interface Output {
     public abstract void promptUser();
     public abstract void displayHelp();
-    public abstract void displayScore();
+    public abstract void displayScore(int[] a);
     public abstract void displayStartup();
     public abstract void displayResult(Result r);
 }
