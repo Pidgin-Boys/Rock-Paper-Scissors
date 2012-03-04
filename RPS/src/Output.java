76ba3bacae995c56befdc444b2fbc52f43286017
Michael Riha <rihamichael@gmail.com>
2012-03-03 21:40:54 -0800

    Added disiplayPrompt();

--- RPS/src/Output.java
+++ RPS/src/Output.java
@@ -1,7 +1,7 @@
 public interface Output {
-    public abstract void promptUser();
     public abstract void displayHelp();
     public abstract void displayScore();
+    public abstract void displayPrompt();
     public abstract void displayStartup();
     public abstract void displayResult(Result r);
 }
