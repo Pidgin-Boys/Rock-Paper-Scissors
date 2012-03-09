df4ef4ef6a0e5e08f7e2c1f47973a5deaeed4201
Lakers2012 <ereedley@yahoo.com>
2012-03-09 07:33:32 -0800

    Update RPS/src/ResultDatabase.java

--- RPS/src/ResultDatabase.java
+++ RPS/src/ResultDatabase.java
@@ -4,11 +4,20 @@ public class ResultDatabase {
     private ArrayList<Result> results;
     private int ties, playerWins, computerWins;
     
+    /*
+     * Creates a new ResultDataBase constructor
+     * Initializes instance varaibles result, ties, playerWins, and computerWins
+     */
     public ResultDatabase() {
         results = new ArrayList<Result>();
         ties = 0; playerWins = 0; computerWins = 0;
     }
 
+    /*
+     * Add a round result to the results data base
+     * @param r - the given round result
+     * @return true if the result was added to result data base, false otherwise
+     */
     public boolean add(Result r) {
         results.add(r);
         int outcome = r.getOutcome();
@@ -26,6 +35,11 @@ public class ResultDatabase {
         return true;
     }
 
+     /*
+     * Gets the score for the current game
+     * @return array containing the number of player wins, computer wins, and ties
+     * for the current game
+     */
     public int[] getScore()
     {
         int[] a = new int[3];
