c44a564df483576086c3c8bd35345a8003744d76
Greg Mathews <gregdmathews@gmail.com>
2012-03-21 19:08:23 -0700

    Now allowing the smart guesser

--- RPS/src/DecisionEngine.java
+++ RPS/src/DecisionEngine.java
@@ -6,7 +6,7 @@ public abstract class DecisionEngine
     public static DecisionEngine makeEngine(String type)
     {
         if (type.equals("random")) return new RandomEngine();
-        //else if (type.equals("smart")) return new SmartEngine();
+        else if (type.equals("smart")) return new SmartEngine();
         //else if (type.equals("predictive")) return new PredictiveEngine();
         else return new RandomEngine(); // default to random
     }
